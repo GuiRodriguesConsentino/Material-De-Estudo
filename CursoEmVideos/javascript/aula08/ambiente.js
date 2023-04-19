75bd1e542fb0adfdfe465e3f051a464fc7a1e134
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-04-19 15:12:25 -0300

    exercicio js

---
+++ CursoEmVideos/javascript/aula08/ambiente.js
@@ -0,0 +1,25 @@
+//USANDO SÓ WHILE (Mais utilizada)
+var c = 1;
+while (c <= 6) {
+  console.log(`Você deu ${c} clicks!`);
+  c++;
+}
+
+
+/*USANDO DO E WHILE
+var c = 1;
+do {
+  console.log(`Você deu ${c} clicks!`);
+  c++;
+} while (c <= 6)
+*/
+
+/* JEITO ERRADO ! ! !
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+*/
