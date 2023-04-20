02648ec0610c6073794bf7b3855d815df74ac443
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-04-20 18:19:48 -0300

    repetição js

---
+++ CursoEmVideos/javascript/aula09/repetição.js
@@ -0,0 +1,13 @@
+console.log('Vai começar...');
+for (var c = 1; c <= 10; c++) {
+   console.log(`Você deu ${c} click!`);
+}
+console.log('Fim!');
+
+/*
+var c = 1
+while (c <= 10) {
+   console.log(`Você deu ${c} click!`);
+   c++
+}
+*/
