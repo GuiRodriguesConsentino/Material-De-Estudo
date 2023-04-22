8473ba5936bb14bf0ab2c73b038460d177583a04
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-04-21 21:06:38 -0300

    terminando exercicio js

--- CursoEmVideos/javascript/aula10/script.js
+++ CursoEmVideos/javascript/aula10/script.js
@@ -6,14 +6,27 @@ function contar() {
 
    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
       alert(`Está faltando um número!`)
+      res.innerHTML = `Impossível contar!`
    } else {
-      res.innerHTML = `Contando:`
+      res.innerHTML = `Contando: <br>`
       var i = Number(ini.value)
       var f = Number(fim.value)
       var p = Number(passo.value)
-
-      for (var c = i; c <= f; c += p) {
-         res.innerHTML += ` ${c}`
+      if (p <= 0) {
+         alert(`Passo invalido! Considerando PASSO 1`)
+         p = 1
       }
+      if (i < f) {
+         // Contagem crescente
+         for (var c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+         }  
+      } else {
+         // Contagem regressiva 
+         for (var c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+         }
+      }  
+      res.innerHTML += ` \u{1F3C1}`  
    }
 }
