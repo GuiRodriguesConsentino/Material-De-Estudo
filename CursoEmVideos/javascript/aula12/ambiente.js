05fdcde38b39a09a6c393c570f94645e99488337
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-04-24 18:14:20 -0300

    aprendendo a saber os valores

--- CursoEmVideos/javascript/aula12/ambiente.js
+++ CursoEmVideos/javascript/aula12/ambiente.js
@@ -5,3 +5,10 @@ num.push(1)
 console.log(num);
 console.log(`Nosso vetor tem ${num.length} posições`);
 console.log(`O primeiro valor do vetor é ${num[0]}`);
+
+let pos = num.indexOf(8) 
+if (pos == -1) {
+   console.log(`Valor não foi encontrado`);
+} else {
+   console.log(`O valor 8 está na posição ${pos}`);
+}
