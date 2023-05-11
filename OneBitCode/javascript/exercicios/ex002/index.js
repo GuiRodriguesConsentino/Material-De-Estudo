1ff1e8fa8c067f14c1e134708585a075e57b18f7
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-05-11 20:44:10 -0300

    exercicio 02 js

---
+++ OneBitCode/javascript/exercicios/ex002/index.js
@@ -0,0 +1,14 @@
+let nome = "Gui"
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+// window.alert(nome)
+// nome = window.prompt('Insira seu nome:')
+// let confirmação = window.confirm('Confirma essa ação?')
+
+// alert('Olá Mundo!')
+// prompt("Olá Mundo!");
+// confirm('Confirme')
+
