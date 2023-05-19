efc23ebfc861892a41d6858ba1cb985eb5a24d82
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-05-19 18:42:05 -0300

    exercicio 09 js parte 2

---
+++ OneBitCode/javascript/exercicios/ex009/array.js
@@ -0,0 +1,9 @@
+let listaDeCompras = ["Arroz", false, 7]
+console.log(listaDeCompras);
+
+listaDeCompras[0] = "Açúcar"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[5] = "Batata"
+console.log(listaDeCompras);
+
+console.log(listaDeCompras[10]);
