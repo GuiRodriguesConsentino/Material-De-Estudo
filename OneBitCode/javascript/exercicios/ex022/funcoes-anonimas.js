1f3a1c679a625e98d80f444cf269c706c5c58893
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-06-01 21:19:03 -0300

    exercicio 022 js parte 1

---
+++ OneBitCode/javascript/exercicios/ex022/funcoes-anonimas.js
@@ -0,0 +1,16 @@
+function somar(a, b) {
+   return a + b
+}
+
+let operação = somar
+
+console.log(operação(4, 5));
+
+// function sem nome
+
+const subtrair = function (a, b) {
+   return a - b
+}
+
+console.log(subtrair(36, 13));
+
