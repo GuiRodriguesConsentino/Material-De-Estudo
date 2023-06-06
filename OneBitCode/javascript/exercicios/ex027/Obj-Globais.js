f32d55f3051ec8c2278967041d091108510fb18c
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-06-06 18:32:34 -0300

    exercicio 027 js parte 2

---
+++ OneBitCode/javascript/exercicios/ex027/Obj-Globais.js
@@ -0,0 +1,34 @@
+Object
+let pessoa = Object()
+console.log(pessoa);
+
+Array
+let lista = Array("A", "B", "C")
+console.log(lista);
+
+String // Converte um número em texto 
+let a = String(42)
+console.log(a);
+
+Number // Converte um número string em número
+let num = Number("42")
+console.log(num);
+
+Number.isInteger // Verificar se o número é inteiro
+
+Number.isFinite // Verificar se o número não é inteiro
+
+Date // Ver datas e horas
+Date()
+
+Math // Ver métodos matemáticos
+
+Math.PI // Valor de PI
+
+Math.pow(2, 10) // Ver a potencia 
+
+Math.sqrt(144) // Raiz Quadrada
+
+Math.random // Números aleatórios
+
+//ETC...
