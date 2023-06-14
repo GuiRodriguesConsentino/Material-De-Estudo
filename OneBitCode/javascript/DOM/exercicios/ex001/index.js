98644d2b80badf5e99e8437d603f2afce0e6f0cb
GuiRodriguesConsentino <Gui.roco007@gmail.com>
2023-06-14 18:06:44 -0300

    exercicio 001 DOM parte 2

---
+++ OneBitCode/javascript/DOM/exercicios/ex001/index.js
@@ -0,0 +1,19 @@
+function mouseEnterEvent() {
+  console.log("Entrada do Mouse");
+}
+
+function changeEvent() {
+  console.log("Mudança do valor");
+}
+
+function focusEvent() {
+  console.log("Foco no elemento");
+}
+
+function blurEvent() {
+  console.log("Desfocando o elemento");
+}
+
+function clickEvent() {
+  alert("clicou");
+}
