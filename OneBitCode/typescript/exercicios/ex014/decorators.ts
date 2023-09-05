cee1d786f5d04b2941eb64b9c4d24b330948c808
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-09-05 19:31:15 -0300

    exercicio 014 TypeScript parte 1

---
+++ OneBitCode/typescript/exercicios/ex014/decorators.ts
@@ -0,0 +1,47 @@
+function Log() { 
+   // Descriptor valor do método
+   // Key nome do método original 
+   return function (target, key, descriptor) {
+      const originalMethod = descriptor.value
+
+      descriptor.value = function (...args: any[]) {
+         console.log('--------------------------------');
+         console.log(`Chamando o método ${key} com os parâmetros: ${JSON.stringify(args)}`);
+       
+         const result = originalMethod.apply(this, args)
+
+         console.log(`O método ${key} retornou o valor: ${JSON.stringify(result)}`);
+         console.log('--------------------------------'); 
+
+         return result
+      }
+   }
+}
+
+class Planet {
+   name: string
+
+   constructo(name: string) {
+      this.name = name
+   }
+
+   @Log()
+   calculate(value: number) {
+      // ...
+      console.log(`Calculando ${value} vezes 2`);
+      return value * 2
+   }
+
+   @Log()
+   invertName() {
+      return this.name.split('').reverse().join('')
+   }
+}
+
+const planet = new Planet('Terra')
+
+const result = planet.calculate(5)
+
+console.log(`Resultado ${result}`);
+
+planet.invertName
