1e4995e224eeb3eb11fa2cf67638fe1a300d09f6
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-09-06 19:30:59 -0300

    exercicio 011 JS Moderno

---
+++ OneBitCode/javascript/Js Moderno/exercicios/ex011/index.js
@@ -0,0 +1,7 @@
+// instalar lodash no node
+
+const lodash = require("lodash")
+
+console.log(lodash.isArray([]));
+console.log(lodash.kebabCase("Guilherme Rodrigues Consentino"));
+// camelCase && kebab-case
