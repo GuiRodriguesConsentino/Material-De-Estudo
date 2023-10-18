62a423d4682f1136f247e5059c169b43e70c3642
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-10-18 20:47:35 -0300

    exercicio 002 react

---
+++ OneBitCode/react/exercicios/ex002/index.jsx
@@ -0,0 +1,16 @@
+// Conhecendo o JSX
+
+const element = <h1>Conhecendo o JSX</h1>
+
+const anotherElement = (
+   <div>
+      <h1>Olá, mundo!</h1>
+      <ul>
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+         <li>4</li>
+         <li>5</li>
+      </ul>
+   </div>
+)
