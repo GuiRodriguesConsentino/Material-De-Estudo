a2aaeda933f6f9514182827c9b993d2e5cc443d8
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-10-19 19:34:35 -0300

    exercicio 003 React

---
+++ OneBitCode/react/exercicios/ex003/index.jsx
@@ -0,0 +1,14 @@
+// Conhecendo os componentes 
+
+function PrimaryButton() {
+   return <button>Isso é um componente!</button>
+}
+
+function Container() {
+   return (
+      <div className="container">
+         <h1>Olá, mundo!</h1>
+         <p>Estou aprendendo react!</p>
+      </div>
+   )
+}
