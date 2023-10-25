b8d6c9446e170e51a970917338d0b80af2231437
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-10-25 20:55:54 -0300

    desafio 001 React parte 2

---
+++ OneBitCode/react/desafios/d001/index.jsx
@@ -0,0 +1,41 @@
+function App() {
+   return (
+     <div>
+       <img src="..." alt="" />
+       <h2>React</h2>
+       <p>A biblioteca para interfaces de usuário web e nativas</p>
+       <div>
+         <button>Aprendendo React</button>
+         <button>Referência da API</button>
+       </div>
+      
+       <hr />
+
+       <h3>Crie interfaces de usuário de componentes</h3>
+       <p>
+         React permite que você construa interfaces de usuários a partir de
+         pedaços individuais chamados componentes
+       </p>
+
+       <hr />
+
+       <h3>Escreva componentes com código e marcação</h3>
+       <p>
+         Componentes React são funções JavaScript. A sintaxe de marcação é
+         chamada JSX. É uma extensão da sintaxe JavaScript popularizada pelo
+         React
+       </p>
+
+       <hr />
+
+       <h3>Próximos passos:</h3>
+       <ul>
+         <li>Uso de dados dinâmicos no JSX</li>
+         <li>Criação de novos componentes</li>
+         <li>Estilização de componentes</li>
+         <li>Uso de props e children</li>
+         <li>Uso de eventos do JavaScript</li>
+       </ul>
+     </div>
+   );
+}
