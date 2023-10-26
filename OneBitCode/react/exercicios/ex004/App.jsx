a12e2ae87f82d2c28c6da673dc0e4ed56685d358
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-10-26 19:44:27 -0300

    pequena alteração no desafio 001 react

--- OneBitCode/react/exercicios/ex004/App.jsx
+++ OneBitCode/react/exercicios/ex004/App.jsx
@@ -1,16 +1,18 @@
+// Exemplo usando o VITE
+
 function App() {
-   return (
-      <div>
-         <h1>Olá, Mundo!</h1>
-         <p>Este é meu primeiro projeto usando React</p>
-         <p>Próximos passos:</p>
-         <ul>
-            <li>Praticar com exercícios</li>
-            <li>Aprender a usar dados dinâmicos</li>
-            <li>Criar novos componentes</li>
-            <li>Estilizar os componentes</li>
-            <li>Reutilizar componentes</li>
-         </ul>
-      </div>
-   )
+  return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Este é meu primeiro projeto usando React</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
+  );
 }
