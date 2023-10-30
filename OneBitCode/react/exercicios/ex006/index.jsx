90940e702a2e190efb4270dd869eafc1ef9832d2
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-10-30 20:03:22 -0300

    exercicio 006 React parte 5

---
+++ OneBitCode/react/exercicios/ex006/index.jsx
@@ -0,0 +1,15 @@
+// Exemplo usando o VITE
+
+import Status from "./components/Status"
+import { Subtitle } from "./components/Subtitle"
+import Title from "./components/Title"
+
+function App() {
+   return (
+      <div>
+         <Title />
+         <Subtitle />
+         <Status />
+      </div>
+   )
+}
