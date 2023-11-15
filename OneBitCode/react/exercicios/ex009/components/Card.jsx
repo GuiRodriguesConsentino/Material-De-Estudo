95671cda7c4965c1d125db43599bcd0bcf117180
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-11-14 21:21:56 -0300

    exercicio 009 React parte 2

---
+++ OneBitCode/react/exercicios/ex009/components/Card.jsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+export default function Card({ title, posterImg }) {
+  return (
+    <div className="styles.container">
+      //Adicionar imagem no assets no VITE *
+      <img className="styles.poster" src={posterImg} alt="..." />
+      <div>
+        <h2>{title}</h2>
+        <p>
+          Um pôster decorativo épico do filme Star Wars, com moldura de MDF e
+          tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de
+          todos os tempos. Este clássico pôster trará aventura, nostalgia e
+          magia de Star Wars para qualquer lugar que você decidir pendurar. Não
+          perca a chance de adicionar essa linda memória ao seu acervo!
+        </p>
+        <button>Comprar agora</button>
+      </div>
+    </div>
+  );
+}
