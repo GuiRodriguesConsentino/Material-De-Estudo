f63f8ba8c4d0346fc8e8bc5741f6f9fa2fae740c
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-12-12 20:50:59 -0300

    exercicio 016 React parte 3

---
+++ OneBitCode/react/exercicios/ex016/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+   return (
+     <header>
+       <nav style={{ display: "flex", gap: "2rem" }}>
+         <Link to="/">Inicio</Link>
+         <Link to="/products">Produtos</Link>
+         <Link to="/cart">Carrinho</Link>
+       </nav>
+     </header>
+   );
+}
