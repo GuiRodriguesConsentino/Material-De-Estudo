b1231e28883be064b6e9152921a0aa8273dd130b
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-12-20 19:49:24 -0300

    exercicio 016 React parte 9

---
+++ OneBitCode/react/exercicios/ex016/pages/Product.jsx
@@ -0,0 +1,13 @@
+export default function Product() {
+   const products = useLoaderData()
+
+  return (
+    <section>
+      <Link to="/products">
+        <button>Voltar</button>
+      </Link>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>R$ {product.price}</p>
+      <button>Comprar</button>
+    </section>
