54c6369a5777e472b6dc4a4d58959337fcc657d6
Guilherme Rodrigues <gui.roco007@gmail.com>
2023-12-27 20:28:43 -0300

    exercicio 016 React parte 13

---
+++ OneBitCode/react/exercicios/ex016/router.jsx
@@ -0,0 +1,44 @@
+// Instalar react router dom
+
+import { createBrowserRouter } from "react-router-dom";
+import AdminHome from "./admin/AdminHome";
+import ProductBoundary from "./error-boundaries/ProductBoundary";
+import loadProduct from "./loaders/products";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
+import RootLayout from "./pages/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+         index: true,
+         element: <Home />
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      }, {
+        path: "products/:productId",
+        element: <Product />,
+        loader: loadProduct,
+        errorElement: <ProductBoundary />
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  }, 
+
+   {
+      path: "/admin",
+      element: <AdminHome />,
+   },
+]);
+
+export default router
