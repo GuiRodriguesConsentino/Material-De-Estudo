adaf01bbff56d77cca8e003f275501aa8faf855e
Guilherme Rodrigues <gui.roco007@gmail.com>
2024-02-05 20:13:52 -0300

    exercicio 001 NodeJs parte 6

---
+++ OneBitCode/node.js/ex001/pages/cart.tsx
@@ -0,0 +1,32 @@
+// // pages/cart.tsx
+
+// import { NextPage } from "next";
+// import Head from "next/head";
+// import CartTable from "../src/components/CartTable"
+// import CartTable from "../src/components/CartTable"
+// import CartTotal from "../src/components/CartTotal"
+
+// const Cart: NextPage = () => {
+//   return (
+//     <>
+//       <Head>
+//         <title>Carrinho</title>
+//         <meta name="description" content="Meu carrinho de compras" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <Headers />
+
+//       <main>
+//         <Container className="mb-5">
+//           <h1 className="my-5">
+//             Carrinho
+//           </h1>
+//           <CartTable />
+//         </Container>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Cart;
