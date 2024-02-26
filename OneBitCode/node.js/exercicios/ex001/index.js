536885748b2e245afcd995b2240d7a9323f430b2
Guilherme Rodrigues <gui.roco007@gmail.com>
2024-02-26 20:31:37 -0300

    exercicio 001 NodeJs parte 3

---
+++ OneBitCode/node.js/exercicios/ex001/index.js
@@ -0,0 +1,11 @@
+// REPL - read-eval-print-loop
+
+const fs = require('node:fs');
+console.log("Inicio");
+
+fs.readFile("./arquivo.txt", 'utf-8', (err, data) => {
+   if (err) throw err;
+   console.log(data);
+});
+
+console.log('Fim');
