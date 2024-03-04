7f7fc4ee4460f77f2ecce92a04abc87380a2117f
Guilherme Rodrigues <gui.roco007@gmail.com>
2024-03-04 20:24:40 -0300

    exercicio 003 NodeJs parte 5

---
+++ OneBitCode/node.js/exercicios/ex003/write-file.js
@@ -0,0 +1,19 @@
+import { fs, writeFileSync } from "node:fs";
+
+const content = "Conteúdo do novo arquivo assíncrono"
+
+fs.writeFile("./arquivo.txt", content, "utf-8", (error) => {
+   if (error) {
+   console.log("Erro ao escrever o arquivo:", error.message);
+   return
+   }
+
+   console.log("Arquivo criado com sucesso de forma assíncrona.");
+})
+
+// try {
+//    writeFileSync("./arquivo.txt", "Olá, mundo!", "utf-8")
+//    console.log("Arquivo criado com sucesso!");
+// } catch (error) {
+//    console.log("Erro ao escrever o arquivo:", error.message);
+// }
