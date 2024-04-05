529c76cde9ac3d6e8c86e5bd9b48e8da8351a5d1
Guilherme Rodrigues <gui.roco007@gmail.com>
2024-04-04 21:24:54 -0300

    exercicio 010 MongoDB parte 4

--- OneBitCode/mongodb/exercicio/ex010/app.js
+++ OneBitCode/mongodb/exercicio/ex010/app.js
@@ -2,12 +2,15 @@ const express = re = require('express');
 const path = require('path')
 
 const checkListRouter = require('./src/routes/checklist');
+const taskRouter = require("./src/routes/task");
+
 const rootRouter = require('./src/routes/index');
 require('./database')
 
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}))
 
 use.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
@@ -15,7 +18,8 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/checklists", rootRouter);
-app.use('/checklists', checkListRouter)
+app.use('/checklists', checkListRouter.checklistDepedent);
+app.use("/checklists", taskRouter);
 
 app.listen(3000, () => {
    console.log('Servidor foi iniciado');
